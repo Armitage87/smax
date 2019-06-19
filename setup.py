03fea6e4d1d4edc563259a9b1eee020ec717ea00
Harvey.Dumancic <dez.osk@gmail.com>
2019-06-19 23:27:15 +0100

    Update Setup.py - update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Harvey Dumancic',
   author_email = 'dez.osk@gmail.com',
   url = 'https://github.com/Armitage87/smax',
-  download_url = 'https://github.com/Armitage87/smax/archive/0.1.tar.gz',
+  download_url = 'https://github.com/Armitage87/smax/archive/v0.1.tar.gz',
   keywords = ['SMAX', 'Service management automation x', 'micro focus', 'automation'],
   install_requires=[
           'requests'
